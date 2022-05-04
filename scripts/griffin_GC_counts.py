837b55e17d3a2bf86a9ec9627a3a36daa32cf39a
Anna-Lisa Doebley <adoebley@uw.edu>
2022-05-04 11:19:12 -0700

    update to revisions1 version

--- scripts/griffin_GC_counts.py
+++ scripts/griffin_GC_counts.py
@@ -16,25 +16,6 @@ import sys
 from multiprocessing import Pool
 
 
-# In[ ]:
-
-
-# ##arguments for testing 
-
-# bam_file_path = '/fh/scratch/delete90/ha_g/realigned_bams/cfDNA_MBC_ULP_hg38/realign_bam_paired_snakemake-master/results/MBC_1041_1_ULP/MBC_1041_1_ULP_recalibrated.bam'
-# bam_file_name = 'MBC_1041_1_ULP'
-# mapable_path = '../../downloads/genome/repeat_masker.mapable.k50.Umap.hg38.bedGraph'
-
-# ref_seq_path = '/fh/fast/ha_g/grp/reference/GRCh38/GRCh38.fa'
-# chrom_sizes_path = '/fh/fast/ha_g/grp/reference/GRCh38/hg38.standard.chrom.sizes'
-
-# out_dir = './tmp/'
-
-# map_q = 20
-# size_range = [15,500]
-
-# CPU = 4
-
 
 # In[ ]:
 
@@ -43,7 +24,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--bam_file', help='sample_bam_file', required=True)
 parser.add_argument('--bam_file_name', help='sample name (does not need to match actual file name)', required=True)
-parser.add_argument('--mapable_regions', help='highly mapable regions to be used in GC correction, bedGraph or bed foramt', required=True)
+parser.add_argument('--mappable_regions_path', help='highly mappable regions to be used in GC correction, bedGraph or bed foramt', required=True)
 
 parser.add_argument('--ref_seq',help='reference sequence (fasta format)',required=True)
 parser.add_argument('--chrom_sizes',help='path to chromosome sizes for the reference seq',required=True)
@@ -59,7 +40,7 @@ args = parser.parse_args()
 
 bam_file_path = args.bam_file
 bam_file_name = args.bam_file_name
-mapable_path=args.mapable_regions
+mappable_regions_path=args.mappable_regions_path
 
 ref_seq_path = args.ref_seq
 chrom_sizes_path = args.chrom_sizes
@@ -77,7 +58,7 @@ print('arguments provided:')
 
 print('\tbam_file_path = "'+bam_file_path+'"')
 print('\tbam_file_name = "'+bam_file_name+'"')
-print('\tmapable_regions = "'+mapable_path+'"')
+print('\tmappable_regions_path = "'+mappable_regions_path+'"')
 
 print('\tref_seq_path = "'+ref_seq_path+'"')
 print('\tchrom_sizes_path = "'+chrom_sizes_path+'"')
@@ -91,34 +72,27 @@ print('\tCPU = '+str(CPU))
 # In[ ]:
 
 
-mapable_name = mapable_path.rsplit('/',1)[1].rsplit('.',1)[0]
-out_file = out_dir +'/'+mapable_name+'/GC_counts/'+ bam_file_name+'.GC_counts.txt'
+out_file = out_dir +'/GC_counts/'+ bam_file_name+'.GC_counts.txt'
 
 print('out_file',out_file)
 
-
-# In[ ]:
-
-
 #create a directory for the GC data
-if not os.path.exists(out_dir +'/'+mapable_name):
-    os.mkdir(out_dir +'/'+mapable_name)
-if not os.path.exists(out_dir +'/'+mapable_name+'/GC_counts/'):
-    os.mkdir(out_dir +'/'+mapable_name+'/GC_counts/')
+if not os.path.exists(out_dir +'/GC_counts/'):
+    os.mkdir(out_dir +'/GC_counts/')
 
 
 # In[ ]:
 
 
 #import filter
-mapable_intervals = pd.read_csv(mapable_path, sep='\t', header=None)
+mappable_intervals = pd.read_csv(mappable_regions_path, sep='\t', header=None)
 
 #remove non standard chromosomes and X and Y
 chroms = ['chr'+str(m) for m in range(1,23)]
-mapable_intervals = mapable_intervals[mapable_intervals[0].isin(chroms)]
+mappable_intervals = mappable_intervals[mappable_intervals[0].isin(chroms)]
 
 print('chroms:', chroms)
-print('number_of_intervals:',len(mapable_intervals))
+print('number_of_intervals:',len(mappable_intervals))
 
 sys.stdout.flush()
 
@@ -150,7 +124,7 @@ def collect_reads(sublist):
         if i%5000==0:
             print('interval',i,':',chrom,start,end,'seconds:',np.round(time.time()-start_time))
             sys.stdout.flush()
-        #fetch any read that overlaps the inteterval (don't need to extend the interval because the fetch function does this automatically)
+        #fetch any read that overlaps the inteterval 
         fetched = bam_file.fetch(chrom,start,end)
         for read in fetched:
             #use both fw (positive template length) and rv (negative template length) reads
@@ -158,45 +132,22 @@ def collect_reads(sublist):
                 #qc filters, some longer fragments are considered 'improper pairs' but I would like to keep these
                 if read.is_paired==True and read.mapping_quality>=map_q and read.is_duplicate==False and read.is_qcfail==False:
                     if read.is_reverse==False:
-                        read_start = read.reference_start
-                        read_end = read.reference_start+read.template_length
+                        fragment_start = read.reference_start
+                        fragment_end = read.reference_start+read.template_length
                     elif read.is_reverse==True:
-                        read_end = read.reference_start + read.reference_length
-                        read_start = read_end + read.template_length
-
-                    fragment_seq = ref_seq.fetch(read.reference_name,read_start,read_end)
-                    #tally up the GC content
-                    fragment_seq=fragment_seq.replace('g','G').replace('c','C').replace('a','A').replace('t','T').replace('n','N')
-
-    #                 #################
-    #                 ##logic check####
-    #                 #################
-    #                 if read.is_reverse==False:
-    #                     if fragment_seq[0:read.reference_length]==read.query_sequence and len(fragment_seq)==read.template_length:
-    #                         print('fw match',read.reference_length)
-    #                     else:
-    #                         print(fragment_seq[0:read.reference_length],read.reference_length,'fw')
-    #                         print(read.query_sequence,len(read.query_sequence),'fw')
-    #                         print(len(fragment_seq),read.template_length)
-    #                         print('\n')
-    #                 elif read.is_reverse==True:
-    #                     if fragment_seq[-read.reference_length:]==read.query_sequence and len(fragment_seq)==-read.template_length:
-    #                         print('rv match',read.reference_length)
-    #                     else:
-    #                         print(fragment_seq[-read.reference_length:],read.reference_length,'rv')
-    #                         print(read.query_sequence,len(read.query_sequence),'rv')
-    #                         print(len(fragment_seq),read.template_length)
-    #                         print('\n')                        
-    #                 #################
-
-                    #split and convert to numpy array
-                    fragment_seq = np.array(list(fragment_seq))
-                    #replace with values
-                    fragment_seq[(fragment_seq=='G') | (fragment_seq=='C')]=1
-                    fragment_seq[(fragment_seq=='A') | (fragment_seq=='T')]=0
-                    fragment_seq[(fragment_seq=='N')]=np.random.randint(2) #choose a random 0 or 1 for N (so that you always get an integer) #should be very rare if the filter is done right
-                    fragment_seq = fragment_seq.astype(int)
-
+                        fragment_end = read.reference_start + read.reference_length
+                        fragment_start = fragment_end + read.template_length
+    
+                    #count the GC content
+                    fragment_seq = ref_seq.fetch(read.reference_name,fragment_start,fragment_end)
+                    fragment_seq = np.array(list(fragment_seq.upper()))
+                    fragment_seq[np.isin(fragment_seq, ['A','T','W'])] = 0
+                    fragment_seq[np.isin(fragment_seq, ['C','G','S'])] = 1
+                    rng = np.random.default_rng(fragment_start)
+                    fragment_seq[np.isin(fragment_seq, ['N','R','Y','K','M','B','D','H','V'])] = rng.integers(2, size=len(fragment_seq[np.isin(fragment_seq, ['N','R','Y','K','M','B','D','H','V'])])) #random integer in range(2) (i.e. 0 or 1)
+                    fragment_seq = fragment_seq.astype(float)
+                    
+                    
                     num_GC = int(fragment_seq.sum())
                     GC_dict[abs(read.template_length)][num_GC]+=1
 
@@ -209,7 +160,7 @@ def collect_reads(sublist):
 
 start_time = time.time()
 p = Pool(processes=CPU) #use the available CPU
-sublists = np.array_split(mapable_intervals,CPU) #split the list into sublists, one per CPU
+sublists = np.array_split(mappable_intervals,CPU) #split the list into sublists, one per CPU
 
 GC_dict_list = p.map(collect_reads, sublists, 1)
 
@@ -240,12 +191,6 @@ all_GC_df.to_csv(out_file,sep='\t',index=False)
 
 
 print('done')
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
